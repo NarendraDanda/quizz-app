0c961b6adb8b19b5fe6c2ea9700d5b6abd6db167
Narendra <narendra2kee@gmail.com>
2024-02-20 23:50:50 +0530

    script file yet to complete

---
+++ script.js
@@ -0,0 +1,30 @@
+const questions = [
+    {
+        question: "which is smallest animal in the world?",
+        answers:[
+            {text: "humming bird", correct: true},
+            {text:"rabbit", correct:false},
+            {text:"elephant", correct:false},
+            {text:"mouse",correct:false},
+        ]
+    }, 
+    {
+        question: "which is smallest animal in the world?",
+        answers:[
+            {text: "humming bird", correct: true},
+            {text:"rabbit", correct:false},
+            {text:"elephant", correct:false},
+            {text:"mouse",correct:false},
+        ]
+    },
+    {
+        question: "which is smallest animal in the world?",
+        answers:[
+            {text: "humming bird", correct: true},
+            {text:"rabbit", correct:false},
+            {text:"elephant", correct:false},
+            {text:"mouse",correct:false},
+        ]
+    }
+];
+
